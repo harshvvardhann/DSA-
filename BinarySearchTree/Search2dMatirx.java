e260894c046b92f6b658d22fdb3691223a08b7a0
harshvvardhann <rajput.harshvardhansingh0504@gmail.com>
2025-02-14 00:00:24 +0530

    binary search

---
+++ BinarySearchTree/Search2dMatirx.java
@@ -0,0 +1,27 @@
+class Search2dMatirx {
+    public boolean binarySearch(int [] a,int target){
+        int low =0;
+        int high = a.length-1;
+        while(low<=high){
+            int mid  = low+(high-low)/2;
+            if(a[mid]==target){
+                return true;
+            }else if(a[mid]>target){
+                high = mid-1;
+            }else{
+                low = mid+1;
+            }
+        }
+        return false;
+    }
+    public boolean searchMatrix(int[][] matrix, int target) {
+        for(int i=0;i<matrix.length;i++){
+           boolean result = binarySearch(matrix[i],target);
+           if(result){
+            return true;
+           }
+        }
+    
+        return false;
+    }
+}
---
+++ BinarySearchTree/validPerfectSquare.java
@@ -0,0 +1,19 @@
+class validPerfectSquare {
+    public boolean isPerfectSquare(int num) {
+        long left = 1;
+        long right = num/2+1;
+
+        while(left<=right){
+            long mid =  left + (right-left)/2;
+            long ans = mid * mid;
+            if(ans == num){
+                return true;
+            }else if(ans>num){
+                right = mid - 1;
+            }else {
+                left = mid + 1;
+            }
+        }
+        return false;
+    }
+}
