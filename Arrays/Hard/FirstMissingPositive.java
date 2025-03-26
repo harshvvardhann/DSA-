1e6b14dd32dcf1ab70443efe75161b213057cd39
harshvvardhann <rajput.harshvardhansingh0504@gmail.com>
2025-03-27 02:17:58 +0530

    .

---
+++ Arrays/Hard/FirstMissingPositive.java
@@ -0,0 +1,26 @@
+package Arrays.Hard;
+
+class FirstMissingPositive {
+    public void swap(int nums[],int i, int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+    public int firstMissingPositive(int[] nums) {
+        int n = nums.length;
+
+        for(int i=0;i<n;i++){
+            while(nums[i]>0 && nums[i]<=n && nums[i]!=nums[nums[i]-1]){
+                swap(nums,i,nums[i]-1);
+            }
+        }
+
+        for(int j=0;j<n;j++){
+            if(nums[j]!=j+1){
+                return j+1;
+            }
+        }
+
+        return n+1;
+    }
+}
