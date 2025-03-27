7159d18fec2a03f83271fe53dee8d18f17a77f56
harshvvardhann <rajput.harshvardhansingh0504@gmail.com>
2025-03-28 01:27:02 +0530

    .

---
+++ Arrays/Medium/CountGoodNumbers.java
@@ -0,0 +1,23 @@
+package Arrays.Medium;
+
+class CountGoodNumbers {
+    public long MOD = 1000000007;
+    public int countGoodNumbers(long n) {
+        long odd = (n/2);
+        long even = (n+1)/2;
+
+        return (int)(pow(5,even) * pow(4,odd) % MOD);
+    }
+
+    public long pow(long x, long num){
+        if(num == 0) return 1;
+
+        long temp = pow(x, num/2);
+
+        if(num%2 == 0){
+            return (temp*temp) % MOD;
+        }else{
+            return (x*temp*temp) % MOD;
+        }
+    }
+}
