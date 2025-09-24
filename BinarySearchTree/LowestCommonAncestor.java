befba1676f413cfa5419b8d1eb8ccee91dcd39b2
harshvvardhann <rajput.harshvardhansingh0504@gmail.com>
2025-09-25 00:10:01 +0530

    BST: Lowest Common Ancestor

---
+++ BinarySearchTree/LowestCommonAncestor.java
@@ -0,0 +1,31 @@
+package BinarySearchTree;
+class LowestCommonAncestor {
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root == null) return null;
+
+        if(root == p || root == q) return root;
+
+        TreeNode leftLCA = lowestCommonAncestor(root.left,p,q);
+        TreeNode rightLCA = lowestCommonAncestor(root.right,p,q);
+
+        if(leftLCA!=null && rightLCA!=null){
+            return root;
+        }else if(leftLCA!=null){
+            return leftLCA;
+        }else{
+            return rightLCA;
+        }
+    }
+}
