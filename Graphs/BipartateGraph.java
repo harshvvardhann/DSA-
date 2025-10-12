acd1d37cc8a6751df845fd0872ab19ad245b5124
harshvvardhann <rajput.harshvardhansingh0504@gmail.com>
2025-10-13 00:50:01 +0530

    minor change in bipartate problem in graphs

--- Graphs/BipartateGraph.java
+++ Graphs/BipartateGraph.java
@@ -5,26 +5,26 @@ import java.util.Queue;
 class BipartateGraph {
     public boolean isBipartite(int[][] graph) {
         int n = graph.length;
-        
+
         int color[] = new int[n];
+        Queue<Integer> queue = new LinkedList<>();
 
         for(int i=0;i<n;i++){
-            if(color[i] != 0) continue; //already colored
-            Queue<Integer> queue = new LinkedList<>();
+            if(color[i] != 0) continue;
             queue.offer(i);
             color[i] = 1;
+
             while(!queue.isEmpty()){
-                int node =  queue.poll();
-                for(int neighbour : graph[node]){
-                    if(color[neighbour] == 0){
-                        color[neighbour] = -color[node];
-                        queue.offer(neighbour);
-                    }else if(color[node] == color[neighbour]){
-                        return false;
-                    }
+                int neighbour = queue.poll();
+                for(int ex :  graph[neighbour]){
+                    if(color[ex] == 0){
+                        color[ex] = -color[neighbour];
+                        queue.offer(ex);
+                    }else if(color[ex] == color[neighbour]) return false;
                 }
             }
-        }
+        }        
+
         return true;
     }
 }
