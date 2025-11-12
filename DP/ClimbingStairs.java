b38b4ecac7524ae0e8f7094cadf82c1c4fc71fb2
harshvvardhann <rajput.harshvardhansingh0504@gmail.com>
2025-11-13 01:17:36 +0530

    DP: Climbing Stairs

---
+++ DP/ClimbingStairs.java
@@ -0,0 +1,16 @@
+package DP;
+class ClimbingStairs {
+    public int climbStairs(int n) {
+        if(n<=1) return 1;
+
+        int a = 1,b = 1;
+
+        for(int i=2;i<=n;i++){
+            int c = a + b;
+            a = b;
+            b = c;
+        }
+        
+        return b;
+    }
+}
